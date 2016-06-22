d65d42872ad759088af40566b5a09f5fdb2512a9
wambuinjoroge <christinenjoroge93@gmail.com>
2016-06-22 14:46:18 -0700

    made a rollback

--- database/migrations/2016_06_20_213855_Customers.php
+++ database/migrations/2016_06_20_213855_Customers.php
@@ -16,7 +16,6 @@ class Customers extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('phone');
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ resources/views/customers/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>dollyshoo</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <table class="table table-bordered">
+        <thead>
+        <tr class="warning">
+            <td>Books</td>
+            <td>Shoes</td>
+            <td>People</td>
+            <td>Classes</td>
+            <td>Designs</td>
+        </tr>
+        </thead>
+</head>
+
+<body>
+<tbody>
+@foreach($customers as $customer)
+  Name
+  Phone
+
+@endforeach
+</tbody>
+</table>
+</body>
+</html>
